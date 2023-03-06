aaf9ae7687f346eda4446b89d0e311ba9b45e9fa
Cirovsky <cirovisqui@gmail.com>
2023-03-06 14:49:38 -0300

    lógica de reconhecimento de voz

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -2,6 +2,18 @@
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
-recognition.start();
-/* const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
-const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent; */
+
+const tentativa = document.querySelector(".tentativa");
+tentativa.onclick = () => recognition.start();
+
+const elResposta = document.querySelector(".resposta");
+let resposta;
+recognition.addEventListener("result", (e) => onSpeak(e));
+
+function onSpeak(e){
+    console.log(e.results)
+    console.log(e.results[0][0].transcript);
+    elResposta.textContent = e.results[0][0].transcript;
+    resposta = e.results[0][0].transcript;
+    return e.results[0][0].transcript;
+}
